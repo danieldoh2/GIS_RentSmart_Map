f3f9a56ced482ddc806746edf35d2420e6450aa6
danieldoh2 <danieldoh.inbox@gmail.com>
2024-07-26 14:09:59 -0500

    Added query_iterator func

--- rentsmart.py
+++ rentsmart.py
@@ -1,14 +1,29 @@
 import json 
 import urllib.request
+import arcpy
+import urllib.parse
+import pandas
 
-url = 'https://data.boston.gov/api/3/action/datastore_search?resource_id=dc615ff7-2ff3-416a-922b-f0f334f085d0&limit=5&q=title:jones'  
-fileobj = urllib.request.urlopen(url)
-print(type(fileobj))
+base_url = "https://data.boston.gov/api/3/action/datastore_search_sql?"
+# sql_query = 'SELECT * from "dc615ff7-2ff3-416a-922b-f0f334f085d0" LIMIT 5'
+sql_query = 'SELECT * from "dc615ff7-2ff3-416a-922b-f0f334f085d0"'
+
+encoded_sql_query = urllib.parse.quote_plus(sql_query)
+
+# Construct the full URL by appending the encoded query
+full_url = f"{base_url}sql={encoded_sql_query}"
+
+# url = 'https://data.boston.gov/api/3/action/datastore_search_sql?sql=SELECT * from "dc615ff7-2ff3-416a-922b-f0f334f085d0" WHERE "title" LIKE \'jones\''
+fileobj = urllib.request.urlopen(full_url)
+# print(type(fileobj))
 response_dict = json.loads(fileobj.read())
 # print(type(response_dict))
+# print(response_dict['result'])
 
-for element in response_dict['result']['fields']:
-    print(element)
-    print(type(element))
-# print(type(response_dict['result']['fields']))
-# print(response_dict.keys())
+print(len(response_dict['result']['records']))
+# for element in response_dict['result']['records']:
+#     print(element)
+#     print(type(element))
+
+
+
