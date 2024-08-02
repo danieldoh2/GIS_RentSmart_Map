20696ad1d0434c5cc42d6fa210d16d5d670d30a7
danieldoh2 <danieldoh.inbox@gmail.com>
2024-08-02 11:04:11 -0500

    fixed errors

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from rentsmart_boston.rentsmart import query_iterator
 from rentsmart_boston.gis_layer_creation import parse_config
-
 import arcpy
 # Initialize a blank list to hold all entries
 blank = []
@@ -8,13 +7,17 @@ blank = []
 # Call the query_iterator function to populate the list
 try:
     all_entries = query_iterator(blank)
-    print("Success!")
+    print("Success with ETL")
 except Exception as e:
     print("error:", e)
 
 
-
 gdb_path = parse_config()
+feature_class_name = "rentsmart_fc"
+gdb_path = f"{gdb_path}/{feature_class_name}"
 print(gdb_path)
+
+print("Buidling FC Into: ", gdb_path)
+
 arcpy.conversion.JSONToFeatures("data.geojson", gdb_path, "POINT" )
 print("Success!")
--- rentsmart_boston/gis_layer_creation.py
+++ rentsmart_boston/gis_layer_creation.py
@@ -1,15 +1,17 @@
-
-import arcpy
 import json
+import os 
 
-with open('config.json', 'r') as file:
-    workplace = json.load(file)
+def parse_config():
+    try:
+        with open('config.json', 'r') as file:
+            workplace = json.load(file)
+        workplace_val = workplace['workspace_var']
+        return workplace_val
+    except Exception as e: 
+        print("Error with parsing!", e)
 
+    # try:
+    #     arcpy.management.CreateFeatureclass(workplace_val, "rentsmart_fc", geometry_type= "POINT")
 
-workplace_val = workplace['workspace_var']
-
-try:
-    arcpy.management.CreateFeatureclass(workplace_val, "rentsmart_fc", geometry_type= "POINT")
-
-except Exception as e:
-    print("Error! :", e)
+    # except Exception as e:
+    #     print("Error! :", e)
